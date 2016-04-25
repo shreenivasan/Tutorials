10dee37fa856bbfbe2616463979955cfc22b3d8c
shreenivas <shreenivas@localhost.localdomain>
2016-04-25 18:41:53 +0530

    files moved

--- php/caching/redis/ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set("display_errors", "On");
-error_reporting(E_ALL);
-$redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-echo "Connection to server sucessfully";
-
-echo "Server is running: ".$redis->ping();
-echo "Server is running: ". $redis->ping();
