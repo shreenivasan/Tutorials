cf8fd4bf337c65de1db9f3781b37e504b745327b
shreenivas <shreenivas@localhost.localdomain>
2017-01-12 15:35:52 +0530

    ERP project added

--- testing/test.php
+++ testing/test.php
@@ -1,4 +1,6 @@
 <?php
+echo json_encode(array('success'=>'File uploaded successfully, You will receive an email on your email id.')); die;
+
 date_default_timezone_set('Asia/Calcutta');
 $cur_date = date('Y-m-d H-i-s');
         
@@ -14,43 +16,46 @@ $h = $time_part[0];
 $mi = $time_part[1];
 $s = $time_part[2];
 
-if($h < 7)
-{
-    $fromDate= $y."-".$m."-".($d-1)." 15:00:01";
-    $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
-    $toDate= $y."-".$m."-".($d)." 07:00:00";
-    $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
-    $where1.=" sfo.updated_at between '".$fromDate."' AND '".$toDate."'";
-}
-elseif($h >=7 && $h<12)
-{
-    $fromDate= $y."-".$m."-".($d)." 07:00:01";
-    $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
-    $toDate= $y."-".$m."-".($d)." 12:00:00";
-    $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
-    $where1.=" sfo.updated_at between '".$fromDate."' AND '".$toDate."'";
-}
-else if($h>=12 && $h<15)
-{
-    $fromDate= $y."-".$m."-".($d)." 12:00:01";
-    $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
-    $toDate= $y."-".$m."-".($d)." 15:00:00";
-    $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
-    $where1.=" sfo.updated_at between '".$fromDate."' AND '".$toDate."'";
-}
-else if($h>=15)
-{
-    $fromDate= $y."-".$m."-".($d)." 15:00:01";
-    $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
-    $toDate= $y."-".$m."-".($d+1)." 07:00:00";
-    $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
-    $where1.=" sfo.updated_at between '".$fromDate."' AND '".$toDate."'";
-}
+$where1=" WHERE tsfoi.status='confirmed' AND ";
+        
+        if($h < 7)
+        {
+            $fromDate= $y."-".$m."-".($d-1)." 15:30:01";
+            $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
+            $toDate= $y."-".$m."-".($d)." 07:00:00";
+            $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
+            $where1.=" sfosh.created_at between '".$fromDate."' AND '".$toDate."'";
+        }
+        elseif($h >=7 && $h<12)
+        {
+            $fromDate= $y."-".$m."-".($d)." 07:00:01";
+            $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
+            $toDate= $y."-".$m."-".($d)." 12:00:00";
+            $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
+            $where1.=" sfosh.created_at between '".$fromDate."' AND '".$toDate."'";
+        }
+        else if($h>=12 && $h<15)
+        {
+            $fromDate= $y."-".$m."-".($d)." 12:00:01";
+            $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
+            $toDate= $y."-".$m."-".($d)." 15:00:00";
+            $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
+            $where1.=" sfosh.created_at between '".$fromDate."' AND '".$toDate."'";
+        }
+        else if($h>=15)
+        {
+            $fromDate= $y."-".$m."-".($d)." 15:30:01";
+            $fromDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($fromDate)));
+            $toDate= $y."-".$m."-".($d+1)." 07:00:00";
+            $toDate=date('Y-m-d H:i:s',strtotime('-330 minutes', strtotime($toDate)));
+            $where1.=" sfosh.created_at between '".$fromDate."' AND '".$toDate."'";
+        }
 
  $query2=" SELECT SUM(sfoi.qty_invoiced) qty, sfoi.sku ";
             $query2.=" FROM sales_flat_order_status_history sfosh ";
             $query2.=" INNER JOIN sales_flat_order sfo on sfosh.parent_id=sfo.entity_id and sfosh.entity_name='invoice'";
-            $query2.=" INNER JOIN sales_flat_order_item sfoi ON sfo.entity_id = sfoi.order_id WHERE ";
+            $query2.=" INNER JOIN sales_flat_order_item sfoi ON sfo.entity_id = sfoi.order_id ";
+            $query2.=" INNER JOIN ts_sales_flat_order_item tsfoi ON tsfoi.item_id = sfoi.item_id ";
             $query2.=$where1." GROUP BY sfoi.sku";
             
             echo $query2; die;
